e40405e6e095f2533141b28db48a881868e3487e
Ali Joveini <alijoveini80@gmail.com>
2025-03-01 00:35:42 +0330

    add window external lib

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 "use client";
+import React, { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    if (typeof window.Bale !== "undefined") {
+      // Code that uses window, only on client-side
+      console.log(window.Bale.initData.user.first_name);
+    }
+  }, []); // Run only once on mount
   return (
     <>
       <h1>Home</h1>
-      <p id="wellcome">
-        {" "}
-        wellcome, {window.Bale ? window.Bale.initData.user.first_name : "user"}
-      </p>
+      <p id="wellcome">wellcome, user</p>
     </>
   );
 }
