a168610937072c2c6ae1648b7727cc9378c70e44
Ali Joveini <alijoveini80@gmail.com>
2025-02-28 23:57:18 +0330

    third

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <p id="wellcome"> wellcome, {window.Bale.initData.user.first_name}</p>
+      {/* <p id="wellcome"> wellcome, {window.Bale.initData.user.first_name}</p> */}
     </>
   );
 }
