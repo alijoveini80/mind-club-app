2d1750e15e07bd92a4e779b8ade21cd13c950a7e
Ali Joveini <alijoveini80@gmail.com>
2025-03-01 17:28:04 +0330

    add ThemeProvider.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ScriptLoader from "@/app/components/ScriptLoader";
+import ThemeProvider from "@/app/components/ThemeProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ScriptLoader />
+        <ThemeProvider />
         {children}
       </body>
     </html>
