e50972e7c1a7ab90fee0320ad116dcc5e2ccd85e
Ali Joveini <alijoveini80@gmail.com>
2025-03-09 23:40:16 +0330

    add import cache

--- src/lib/dal.js
+++ src/lib/dal.js
@@ -2,7 +2,8 @@ import "server-only";
 
 import { cookies } from "next/headers";
 import { decrypt } from "@/lib/session";
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
+import { cache } from "react";
 
 export const verifySession = cache(async () => {
   const cookie = (await cookies()).get("session")?.value;
