1273e98e06285a73d14eee62aba9e50ebe727e7d
Ali Joveini <alijoveini80@gmail.com>
2025-03-10 00:40:29 +0330

    session route edited

--- src/app/api/auth/session/route.js
+++ src/app/api/auth/session/route.js
@@ -5,19 +5,23 @@ import { verifySession } from "@/lib/dal";
 export async function GET() {
   const session = await verifySession();
   if (!session) {
-    return new NextResponse.json(null, { status: 401 });
+    return NextResponse.json(null, { status: 401 });
   }
-  return new NextResponse.json(
+  return NextResponse.json(
     { ok: true, userid: session.userid },
     { status: 200 }
   );
 }
 
 export async function POST(req) {
-  const { userid } = await req.json();
-  await createSession(userid);
+  try {
+    const { userid } = await req.json(); // Correct way to parse JSON
+    await createSession(userid);
 
-  return NextResponse.json({ ok: true }, { status: 200 });
+    return NextResponse.json({ ok: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+  }
 }
 
 export async function DELETE() {
