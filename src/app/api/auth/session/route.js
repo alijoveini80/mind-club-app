b2b50506f4a4fa7cf614fee084ef15d2e011b797
Ali Joveini <alijoveini80@gmail.com>
2025-03-10 02:35:55 +0330

    add try catch

--- src/app/api/auth/session/route.js
+++ src/app/api/auth/session/route.js
@@ -3,33 +3,41 @@ import { NextResponse } from "next/server";
 import { verifySession } from "@/lib/dal";
 import { cookies } from "next/headers";
 
-export async function GET() {
-  // const session = await verifySession();
-  const cookieStore = await cookies();
-  const cookie = cookieStore.get("session")?.value;
+export async function GET(request) {
+  try {
+    // const session = await verifySession();
+    // const cookieStore = await cookies();
+    // const cookie = cookieStore.get("session")?.value;
+    const cookie = request.cookies.get(session)?.value;
 
-  // If the cookie doesn't exist, redirect immediately
-  if (!cookie) {
-    return NextResponse.json({ error: "cookie not found" }, { status: 401 });
-  }
+    // If the cookie doesn't exist, redirect immediately
+    if (!cookie) {
+      return NextResponse.json({ error: "cookie not found" }, { status: 401 });
+    }
 
-  const session = await decrypt(cookie);
+    const session = await decrypt(cookie);
 
-  // If decryption fails or session is invalid, redirect
-  if (!session || !session.userid) {
+    // If decryption fails or session is invalid, redirect
+    if (!session || !session.userid) {
+      return NextResponse.json(
+        { error: "decryption fails or session is invalid" },
+        { status: 401 }
+      );
+    }
+
+    // if (!session) {
+    //   return NextResponse.json({ error: "not found" }, { status: 401 });
+    // }
     return NextResponse.json(
-      { error: "decryption fails or session is invalid" },
-      { status: 401 }
+      { ok: true, userid: session.userid },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Invalid request", er: error },
+      { status: 400 }
     );
   }
-
-  // if (!session) {
-  //   return NextResponse.json({ error: "not found" }, { status: 401 });
-  // }
-  return NextResponse.json(
-    { ok: true, userid: session.userid },
-    { status: 200 }
-  );
 }
 
 export async function POST(req) {
