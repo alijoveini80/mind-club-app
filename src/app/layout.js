dd7059b00d9a242b957c57aa4a5543622636bf3b
Ali Joveini <alijoveini80@gmail.com>
2025-03-09 23:35:30 +0330

    delete fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,17 @@
 import ScriptLoader from "@/app/components/ScriptLoader";
 import ThemeProvider from "@/app/components/ThemeProvider";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "Create Next App",
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ScriptLoader />
         {children}
